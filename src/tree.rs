3d9e8fccea9ccc9e62e04304e2ba0a74b394c69a
Leo <isidororossinia@gmail.com>
2025-04-07 13:51:58 -0400

    Started 2-3-4 tree structure.
    Co-authored-by: Clover B.-P. Johnson <callmeclover@clovr.rocks>

--- src/tree.rs
+++ src/tree.rs
@@ -1,1 +1,16 @@
 //! Implementation of a 2-3-4 tree.
+
+pub struct Tree234<'a, T> {
+    pub children: Vec<Node234<'a, T>>
+}
+
+enum Node234<T> {
+    Node2(Node2<T>),
+    Node3(Node3<T>),
+    Node4(Node4<T>),
+}
+
+pub struct Node2<'a, T> {
+    pub children: [&'a Node234<T>; 2],
+    pub elements: T
+}
